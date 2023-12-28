2355cdc08990d7aa384e3a1cb36949205197eb2c
allaner <atj3jjj@hotmail.com>
2023-12-27 22:26:20 -0600

    Fixed Projectile Shooting

--- Scripts/Classes/Projectile.gd
+++ Scripts/Classes/Projectile.gd
@@ -1,6 +1,21 @@
 extends Sprite2D
 class_name Projectile
 
+var Speed = 250
+var Target = null
+var attackDMG: int
 
-func _ready():
-	pass
+func _physics_process(delta):
+	var direction = Vector2.RIGHT.rotated(rotation)
+	global_position += Speed * direction * delta
+
+
+func _on_timer_timeout():
+	queue_free()
+
+
+func _on_area_2d_body_entered(body):
+	if body == Target:
+		Target.HP -= attackDMG
+		print("Dealt ", attackDMG," damage. ", Target.HP)
+		queue_free()
--- Scripts/Classes/RangedUnit.gd
+++ Scripts/Classes/RangedUnit.gd
@@ -1,23 +1,33 @@
 extends Unit
 class_name Ranged_Unit
 
-enum ProjectileType {
-	SorcSpell,
-	XbowArrow,
-	Arrow,
-	ThrowingHammer
-}
 
-@export var current_projectile_type: ProjectileType
+@onready var projectile = preload("res://Scenes/Components/SorcSpell.tscn")
 
-
-var projectiles: Dictionary = {
-	ProjectileType.SorcSpell: preload("res://Scenes/Components/SorcSpell.tscn")
-}
 
 func Ranged_Attack():
 	isattacking = true
-	
-	
-	
+	var instancedir = self.global_position.direction_to(Target.global_position)
+	var instance = projectile.instantiate()
+	add_child(instance)
+	instance.global_position = self.global_position
+	var instancerotation = instancedir.angle()
+	instance.rotation = instancerotation
+	instance.Target = Target
+	instance.attackDMG = attackDMG
 	$AttackRange/AttackTimer.start()
+
+
+#func FireballAttack():
+#
+#	var instancedir = self.global_position.direction_to(Target.global_position)
+#	var instance = Fireball.instantiate()
+##	get_tree().current_scene.add_child(instance)
+#	add_child(instance)
+#	instance.global_position = self.global_position
+#	var instancerotation = instancedir.angle()
+#	instance.rotation = instancerotation
+#	instance.isenemy = true
+#	instance.Attackdamage = $AttackComp.Attackdamage
+#	$AttackComp/FireballTimer.start()
+#	isattacking = true
